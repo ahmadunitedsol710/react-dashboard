d86da3e8eb6bc79f2a37c04d4c047cb3e87ea2f8
Ahmad <ahmad.kamal@unitedsol.net>
2024-08-01 12:36:22 +0500

    Error

--- .history/src/App_20240801122721.js
+++ .history/src/App_20240801122721.js
@@ -1,6 +1,6 @@
 // import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './Components/Header/index';
-import Events from './Components/Body/Events';
+import Eve
 import './App.css';
 
 function App() {
---
+++ .history/src/Components/Body/upcoming-event_20240801113755.js
@@ -0,0 +1,48 @@
+import emptyfav from '../../Images/empty_fav.png'
+
+const Upcommingevent = (eventdata, setStoredData, favdata) => {
+    const add_fav = (data) => {
+        const isFav = favdata.some(favitem => favitem.id === data.id)
+
+        if (isFav) {
+            setStoredData(favdata.filter(favitem => favitem.id !== data.id));    
+            // localStorage.setItem("not favourite", JSON.stringify(favdata))
+            localStorage.removeItem(JSON.stringify(favdata))
+            console.log("else 1")
+        } 
+        else {
+            // Add the item if it does not exist
+            setStoredData(prevFavarr => [...prevFavarr, data]);
+            localStorage.setItem("favourite", JSON.stringify(favdata))
+            console.log("else 2")
+        }
+    };
+    
+    return (
+        <>  {/* add multiple tags when use empty tags */}
+            <div className="upcomingevents">
+                <h2 className="title">Upcoming Events</h2>
+
+                <ul className="events_list">
+                    {eventdata.map((list) => {
+                        return (
+                        <li className="event" key={list.id}>
+                            <div className="title_date_time">
+                                <p className="event_title">{list.name}</p>
+                                <p className="event_date_time">{list.date_time}</p>
+                            </div>
+                            <div>
+                                <img src={list.action} alt="emptyfav" />
+                            </div>
+                        </li>
+                        )
+                    })}
+
+                </ul>
+            </div>
+        </>
+    )
+
+}
+
+export default Upcommingevent;
